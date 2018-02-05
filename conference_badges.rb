188ff4ff8b7b6d76a7ba1df7fff86405d7f9aec8
J B <ironviralmedia@gmail.com>
2018-02-05 17:22:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
+  badges = []
   names.each do |name|
-    return "Hello, my name is #{name}."
+    badges << "Hello, my name is #{name}."
   end
+  badges
 end
