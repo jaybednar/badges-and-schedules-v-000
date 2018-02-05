2ca7a1084fb20fc7ffa54ba09eb052979204a9db
J B <ironviralmedia@gmail.com>
2018-02-05 17:26:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,10 +13,13 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names)
+  rooms = []
   names.each_with_index do |name, index|
-  "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  rooms
 end
 
 def printer
 
-end 
+end
